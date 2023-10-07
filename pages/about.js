9e6ddaf810dc8c846372591cdfe0ccf355b3e750
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2023-10-07 08:20:10 +0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -14,10 +14,8 @@ export default function About() {
                 <title>{siteTitle}</title>
             </Head>
             <br />
-            &emsp;&emsp;This website is built on <Link href={"https://react.dev/"}>React</Link> + <Link href={"https://nextjs.org/"}>Next.js</Link> with few CSS, which is a tribute to <Link href={"https://bellard.org/"}>Fabrice Bellard</Link> whom is my inspiration.<br/>
+            &emsp;&emsp;This website is built on <Link href={"https://react.dev/"}>React</Link> + <Link href={"https://nextjs.org/"}>Next.js</Link> with few CSS. Inspiration: <Link href={"https://bellard.org/"}>Fabrice Bellard</Link>.<br/>
             &emsp;&emsp;If you have any questions or suggestions, write to <Link href={"mailto:cirnovskyv@gmail.com"}>cirnovskyv@gmail.com</Link>.<br/>
         </Layout>
     )
 }
-
-
