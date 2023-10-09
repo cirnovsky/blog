a8f25c471fbe3a1020344adc8ee5e7360530ead1
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2023-10-09 08:29:28 +0800

    Update posts.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import React from "react";
+import localFont from 'next/font/local'
 import '../styles/global.css'
 
+const myFont = localFont({
+    src: './french-script-mt.ttf'
+})
+
 function MyApp({Component, pageProps}) {
-    return <Component {...pageProps}/>
+    return (
+        <main className={myFont.className}>
+            <Component {...pageProps}/>
+        </main>
+    )
 }
 
 export default MyApp
