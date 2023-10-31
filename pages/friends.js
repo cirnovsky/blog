f3406217e15317e257f96ee46845e5464ec4f077
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2023-10-31 17:13:41 +0800

    update friends

--- pages/friends.js
+++ pages/friends.js
@@ -18,7 +18,9 @@ export default function Friends() {
             <div className='content dark:text-gray-300'>
                 <p>This page is under progress.</p>
                 <ul>
-                    <li>Isaac Jin - <Link href="https://www.whale3ye.com/"><italic>CHAOS</italic></Link></li>
+                    <li>cqbzly - <Link href="https://duanyu.netlify.app/"><i>duanyu's blog</i></Link> / <Link href="https://blog.csdn.net/cqbzlydd"><i>duanyu's blog (CSDN)</i></Link></li>
+                    <li>Raisetsu41 - <Link href="https://iridescent41.cf/"><i>Raisetsu41's blog</i></Link></li>
+                    <li>Isaac Jin - <Link href="https://www.whale3ye.com/"><i>CHAOS</i></Link></li>
                 </ul>
             </div>
             <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
