8a0631ba07859aa7658ee488cf9ea63e0441634f
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2023-11-04 14:49:36 +0800

    fix comments.

--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -4,55 +4,63 @@ import Head from "next/head";
 import Date from "../../components/Date";
 import React from "react";
 import { Sections } from "../../lib/enums";
-import Giscus from "@giscus/react";
 
 const section = Sections.NOTES;
 
 export default function Post({ postData }) {
-    let curPostData = postData.content;
-    return (<Layout section={section}>
-        <Head>
-            <title className="text-left"> {curPostData.rawTitle}</title>
-        </Head>
-        <div>
-            <div className="text-[40px] dark:text-gray-300" dangerouslySetInnerHTML={{ __html: curPostData.title }} />
-            <div className="light-text">
-                cirnovsky / <Date dateString={postData.date} />
-            </div>
-            <div className="content dark:text-gray-300" dangerouslySetInnerHTML={{ __html: curPostData.contentHtml }} />
+  let curPostData = postData.content;
+  return (
+    <Layout section={section}>
+      <Head>
+        <title className="text-left"> {curPostData.rawTitle}</title>
+      </Head>
+      <div>
+        <div
+          className="text-[40px] dark:text-gray-300"
+          dangerouslySetInnerHTML={{ __html: curPostData.title }}
+        />
+        <div className="light-text">
+          cirnovsky / <Date dateString={postData.date} />
         </div>
-        <Giscus src="https://giscus.app/client.js"
-            repo="cirnovsky/blog-comments"
-            repo-id="R_kgDOKmz0Wg"
-            category="[ENTER CATEGORY NAME HERE]"
-            category-id="[ENTER CATEGORY ID HERE]"
-            mapping="pathname"
-            strict="0"
-            reactions-enabled="1"
-            emit-metadata="0"
-            input-position="top"
-            theme="transparent_dark"
-            lang="en"
-            loading="lazy"
-            crossorigin="anonymous"
-            async>
-        </Giscus>
-    </Layout>)
+        <div
+          className="content dark:text-gray-300"
+          dangerouslySetInnerHTML={{ __html: curPostData.contentHtml }}
+        />
+      </div>
+      <script
+        src="https://giscus.app/client.js"
+        data-repo="cirnovsky/blog-comments"
+        data-repo-id="R_kgDOKmz0Wg"
+        data-category="General"
+        data-category-id="DIC_kwDOKmz0Ws4Caiud"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="top"
+        data-theme="transparent_dark"
+        data-lang="en"
+        data-loading="lazy"
+        crossorigin="anonymous"
+        async
+      ></script>
+    </Layout>
+  );
 }
 
 export const getStaticPaths = async () => {
-    const paths = getAllPostIds(section);
-    return {
-        paths, fallback: false,
-    };
-}
-
+  const paths = getAllPostIds(section);
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export const getStaticProps = async ({ params }) => {
-    const postData = await getPostData(section, params?.id)
-    return {
-        props: {
-            postData,
-        }
-    };
-}
+  const postData = await getPostData(section, params?.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
