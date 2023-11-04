5f65e4b12e331c21853e66ca24ff2060f6ae43d4
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2023-11-04 09:18:27 +0800

    employ prism.js. bugs there

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
+        />
+        <link
+          href={`https://unpkg.com/prism-themes@1.6.0/themes/prism-ghcolors.css`}
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
