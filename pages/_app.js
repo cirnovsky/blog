9a5f4cc57fda7186b6215a88851bbc7b7e82a3e0
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2023-11-04 14:04:23 +0800

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import React from "react";
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
-function MyApp({Component, pageProps}) {
-    return (
-        <main>
-            <Component {...pageProps}/>
-        </main>
-    )
+function MyApp({ Component, pageProps }) {
+  return (
+    <main>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
