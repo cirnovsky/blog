b75a56594f46f0d453ffccd626c9bcdc17151a08
Carnicero Irnobkowski <cirnovskyv@gmail.com>
2023-11-17 17:40:25 +0800

    font update; using yihui

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,8 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
           rel="stylesheet"
         />
-        <link href="../prism/themes/ghcolors.css" rel="stylesheet" />
-        <link href="../prism/themes/plugins.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/gh/cirnovsky/cdn/prism/themes/ghcolors.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/gh/cirnovsky/cdn/prism/themes/plugins.css" rel="stylesheet" />
       </Head>
       <body>
         <script src="../prism/prism.js" />
