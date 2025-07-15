d12871a3a29ae652369808bc0acbb82ac08228b2
cirnovsky <cirnovskyv@gmail.com>
2025-07-15 10:47:43 +0800

    put posts to homepage; category by year

--- src/js/file.js
+++ src/js/file.js
@@ -7,16 +7,15 @@ function formatDate(s) {
     const mm = s.slice(4, 6)
     const dd = s.slice(6, 8)
 
-    const date = new Date(YY, mm, dd)
+    const date = new Date(YY, parseInt(mm) - 1, dd)
 
+    console.log(YY, mm, dd, date)
     const options = { day: '2-digit', month: 'short', year: 'numeric' }
     return date.toLocaleDateString('en-GB', options)
 }
 
-function pagination(ent) {
-    const start = (currentPage - 1) * ENTRIES_PER_PAGE
-    const end = currentPage * ENTRIES_PER_PAGE
-    $("#entries").html(`<table>
+function pagination(ent, year, start, end) {
+    $("#entries").append(`<h1>${year}</h1><table>
         <thead>
             <tr>
                 <th>Date</th>
@@ -25,31 +24,39 @@ function pagination(ent) {
                 <th>Tags</th>
             </tr>
         </thead>
-        <tbody></tbody>
+        <tbody id="${year}"></tbody>
     </table>`)
 
     const tagsToLinks = (tags) => tags.map(tag => `<a href="/tags.html?tag=${tag}">${tag}</a>`)
 
-    for (let i = start; i < Math.min(end, ent.length); ++i)
-        $(`tbody`).append(`<tr><td>${formatDate(ent[i].date)}</td><td>${ent[i].title}<td><a href="${ent[i].category}/${ent[i].slug}.html">${ent[i].slug}</a></td></td><td class="no-wrap">${tagsToLinks(ent[i].tags)}</td></tr>`)
+    for (let i = start; i < end; ++i)
+        $(`#${year}`).append(`<tr><td>${formatDate(ent[i].date)}</td><td>${ent[i].title}<td><a href="${ent[i].category}/${ent[i].slug}.html">${ent[i].slug}</a></td></td><td class="no-wrap">${tagsToLinks(ent[i].tags)}</td></tr>`)
 }
 
 function parseEntries(ent, params="") {
     // Dangerous if linux!!!!
-    ent = ent.filter(x => ((x.category == currentBranch) || (currentBranch != "notes" && currentBranch != "articles")));
-    totalPage = Math.ceil(ent.length / ENTRIES_PER_PAGE)
-    pagination(ent)
+    // ent = ent.filter(x => ((x.category == currentBranch) || (currentBranch != "notes" && currentBranch != "articles")));
+    // totalPage = Math.ceil(ent.length / ENTRIES_PER_PAGE)
+    // pagination(ent)
 
-    let pages = new Array()
-    for (let i = Math.max(currentPage - 3, 0); i < Math.min(currentPage + 3, totalPage); ++i)
-        pages.push(i + 1)
-    console.log(`${pages.length}, ${totalPage}`)
-    $("#pageid").append(`<button onclick="location.href='${currentBranch}.html?${params}&page=${1}'">&lt;&lt;</button>`)
-    for (let i of pages) {
-        $("#pageid").append(`<button onclick="location.href='${currentBranch}.html?${params}&page=${i}'" class="${i==currentPage?'currentPage':''}">${i}</button>`)
+    // let pages = new Array()
+    // for (let i = Math.max(currentPage - 3, 0); i < Math.min(currentPage + 3, totalPage); ++i)
+    //     pages.push(i + 1)
+    // console.log(`${pages.length}, ${totalPage}`)
+    // $("#pageid").append(`<button onclick="location.href='${currentBranch}.html?${params}&page=${1}'">&lt;&lt;</button>`)
+    // for (let i of pages) {
+    //     $("#pageid").append(`<button onclick="location.href='${currentBranch}.html?${params}&page=${i}'" class="${i==currentPage?'currentPage':''}">${i}</button>`)
+    // }
+    // $("#pageid").append(`<button onclick="location.href='${currentBranch}.html?${params}&page=${totalPage}'">&gt;&gt;</button><br>`)
+    // $("#totpage").text(totalPage)
+    let pos = 0
+    for (let i = 1; i <= ent.length; ++i) {
+        if (i == ent.length || ent[i].date.slice(0, 4) != ent[pos].date.slice(0, 4)) {
+            console.log(pos, i, ent[pos].date.slice(0, 4))
+            pagination(ent, ent[pos].date.slice(0, 4), pos, i)
+            pos = i
+        }
     }
-    $("#pageid").append(`<button onclick="location.href='${currentBranch}.html?${params}&page=${totalPage}'">&gt;&gt;</button><br>`)
-    $("#totpage").text(totalPage)
 }
 
 (function () {
